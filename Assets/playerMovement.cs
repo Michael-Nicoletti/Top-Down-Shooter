cac8dd11eebb31e95ecda0d37eec61d08a70c38d
Michael-Nicoletti <mike.e.nicoletti@gmail.com>
2015-07-31 12:45:17 -0400

    Opening Commit
    Rotation Controls 50% added

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+    private Camera gCam;
+    private Vector2 mousePos;
+
+    [SerializeField] GameObject player;
+
+	// Use this for initialization
+	void Start () 
+    {
+        gCam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            mouseHandler();
+        }
+	
+	}
+
+    private void mouseHandler()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(gCam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if(hit.collider != null)
+        {
+            if(hit.collider.tag == "ground")
+            {
+                Vector3 targetPosition = gCam.ScreenToWorldPoint(Input.mousePosition);
+
+                Debug.Log(targetPosition);
+
+                transform.LookAt(new Vector3(targetPosition.x, targetPosition.y, -10));
+
+                Debug.DrawLine(targetPosition, player.transform.position);
+
+            }
+        }
+    
+    }
+}
