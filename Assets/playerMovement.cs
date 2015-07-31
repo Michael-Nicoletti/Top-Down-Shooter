e8c70d1051b77342f0a71e39cc5704ab8b7eda08
Michael Nicoletti <mike.e.nicoletti@gmail.com>
2015-07-31 18:13:10 -0400

    Rotation and Movement
    Movement has been added, needs limiting on screen edge and when target
    point has been hit

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -5,13 +5,16 @@ public class playerMovement : MonoBehaviour {
 
     private Camera gCam;
     private Vector2 mousePos;
+    private Rigidbody2D rb;
 
     [SerializeField] GameObject player;
+    [SerializeField] float moveSpeed;
 
 	// Use this for initialization
 	void Start () 
     {
         gCam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -31,13 +34,26 @@ public class playerMovement : MonoBehaviour {
         {
             if(hit.collider.tag == "ground")
             {
-                Vector3 targetPosition = gCam.ScreenToWorldPoint(Input.mousePosition);
+                rb.velocity = Vector3.zero;
+                /*Vector3 targetPosition = gCam.ScreenToWorldPoint(Input.mousePosition);
+
+                
 
                 Debug.Log(targetPosition);
 
-                transform.LookAt(new Vector3(targetPosition.x, targetPosition.y, -10));
+                Debug.DrawLine(targetPosition, player.transform.position);*/
 
-                Debug.DrawLine(targetPosition, player.transform.position);
+                Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+                Vector3 dir = Input.mousePosition - pos;
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+                if(Vector3.Distance(transform.position, dir) > 1)
+                {
+                    rb.AddForce(transform.right * moveSpeed);
+                }
+
+                Debug.DrawLine(pos, dir);
 
             }
         }
